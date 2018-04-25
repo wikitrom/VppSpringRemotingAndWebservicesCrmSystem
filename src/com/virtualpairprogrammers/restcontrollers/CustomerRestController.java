fbe999e31a369ba30496cf9b40d45f5e955498fe
Mats Bergstroem <hackingtrom@gmail.com>
2018-04-25 09:57:49 +0200

    Return HTTP 404:NOT FOUND for non-existing entries, approach 1
    This commit contains the first approach described below.
    
    Spring allows for different approaches on how to control the return of
    HTTP codes to the requesting client:
    
    1. Upgrade the return type by wrapping the return type in a Spring
       ResponseEntity<T> type which in turn includes the object and an
       http code.
       Might be seen as a little bit ugly but usually works fine.
    
    2. Let Spring handle the http code by making your object throw an
       exception that Spring can map to appropriate HTTP error code.
       This second approach could be implemented in different ways,
       some nice some ugly or invasive.

--- src/com/virtualpairprogrammers/restcontrollers/CustomerRestController.java
+++ src/com/virtualpairprogrammers/restcontrollers/CustomerRestController.java
@@ -1,6 +1,8 @@
 package com.virtualpairprogrammers.restcontrollers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,17 +18,20 @@ public class CustomerRestController {
 
 	// we want to support GETs to /customer/373737
 	@RequestMapping(value = "/customer/{id}")
-	public Customer findCustomerById(@PathVariable String id) {
+	public ResponseEntity<Customer> findCustomerById(@PathVariable String id) {
 		Customer foundCustomer;
 		try {
 			foundCustomer = customerService.getFullCustomerDetail(id);
 		} catch (CustomerNotFoundException e) {
-			// TODO - improve this
-			throw new RuntimeException(e);
+			// return HTTP 404 to requesting client
+			// to ways to do this
+			// 1. upgrade the return type -> UGLY!
+			// 2. set appropriate http header -> selected!
+				return new ResponseEntity<Customer>(HttpStatus.NOT_FOUND);
 		}
 
 		// the returned object will be picked up by an httpmessageconverter (if a
 		// suitable one exist) to fulfill the content type requested by the client
-		return foundCustomer;
+		return new ResponseEntity<Customer>(foundCustomer, HttpStatus.OK);
 	}
 }
