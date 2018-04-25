10e8a7c7566a35da131e3cdde18b49fba8d455a1
Mats Bergstroem <hackingtrom@gmail.com>
2018-04-25 10:39:07 +0200

    Return HTTP 404:NOT FOUND for non-existing entries, approach 2.2
    This commit uses the second approach described below.
    
    The commit shows a different, less invasive way to implement the
    second approach compared to the previous commit. It does not
    'infect' the Exception (POJO) class with any Spring annotations.
    
    Note that this impementation still returns a http:404 html page
    suitable for humans, *not* for machine-machine communication.
    
    Spring allows for different approaches on how to control the return of
    HTTP codes to the requesting client:
    
    1. Upgrade the return type by wrapping the return type in a Spring
       ResponseEntity<T> type which in turn includes the object and an
       http code.
       Might be seen as a little bit ugly but usually works fine.
    
    2. Let Spring handle the http code by making your object throw an
       exception that Spring can map to appropriate HTTP error code.
       This second approach could be implemented in different ways,
       some nice some ugly or invasive.

--- src/com/virtualpairprogrammers/restcontrollers/CustomerRestController.java
+++ src/com/virtualpairprogrammers/restcontrollers/CustomerRestController.java
@@ -1,8 +1,11 @@
 package com.virtualpairprogrammers.restcontrollers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.virtualpairprogrammers.domain.Customer;
@@ -14,6 +17,12 @@ public class CustomerRestController {
 	@Autowired
 	private CustomerManagementService customerService;
 
+	// use Spring error handling support
+	@ResponseStatus(value=HttpStatus.NOT_FOUND, reason="The customer was not found")
+	@ExceptionHandler(CustomerNotFoundException.class)
+	public void  rulesForCustomerNotFound() {};
+	
+	
 	// we want to support GETs to /customer/373737
 	@RequestMapping(value = "/customer/{id}")
 	public Customer findCustomerById(@PathVariable String id)
